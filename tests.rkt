416736de7c9cff7f5f19dbe9074634aa9f81e1cb
vendethiel <vendethiel@hotmail.fr>
2015-08-12 00:09:09 +0200

    Prepare ground for the parser!

--- tests.rkt
+++ tests.rkt
@@ -1,6 +1,8 @@
 #lang racket
 (require rackunit rackunit/text-ui)
-(require "tests/lexer.rkt")
+(require "tests/lexer.rkt" "tests/parser.rkt")
 
 (display "# lexer\n")
 (run-tests lexer-tests)
+(display "# parser\n")
+(run-tests parser-tests)
