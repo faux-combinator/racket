bd5afd11c7d9ef18101689af225fc7d9e28f8666
vendethiel <vendethiel@hotmail.fr>
2015-08-11 12:46:42 +0200

    lexer implementation

---
+++ lexer.rkt
@@ -0,0 +1,22 @@
+#lang racket
+(provide lex)
+
+(define (make-pattern pattern)
+  (regexp (string-append "^(" pattern ")")))
+
+(define (lex patterns code)
+  (let loop ([code code] [tokens '()])
+    (if (= 0 (string-length code))
+        (reverse tokens) ; as the cons is backwards...
+        (if (eq? #\space (string-ref code 0))
+            (loop (string-trim code) tokens)
+            (match (for/first ([pattern patterns]
+                    #:when (regexp-match? (make-pattern (car pattern)) code))
+                    pattern)
+              [(list regxp type)
+               (match-let ([(list _ m) (regexp-match (make-pattern regxp) code)])
+                 (loop (substring code (string-length m))
+                       (cons (list type m) tokens)))]
+              [#f (raise (string-append
+                          "Unable to parse code: "
+                          (substring code 0 (min 15 (string-length code)))))])))))
