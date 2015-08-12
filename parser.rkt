dd0562bff7142ead017c35059feff6a6c7ed4b46
vendethiel <vendethiel@hotmail.fr>
2015-08-13 01:14:44 +0200

    Implement "maybe"
    still using a thunking macro... looks good otherwise...

--- parser.rkt
+++ parser.rkt
@@ -1,11 +1,19 @@
 #lang racket
 (provide make-parser
-         expect)
+         expect maybe
+         exn:parser-error?)
 
 ; I'd rather this was not mutable, alas,
-;  I don't know how to do this and keep parameters
+;  I don't know how to do this and keep parameters...
+;  It'd solve many issues, however (as in maybe)
 (struct parser-impl (tokens) #:mutable)
 (struct exn:parser-error exn ())
+
+(define (get-cur-tokens)
+  (parser-impl-tokens (current-parser)))
+
+(define (set-cur-tokens! tokens)
+  (set-parser-impl-tokens! (current-parser) tokens))
 
 (define current-parser (make-parameter null))
 
@@ -20,11 +28,22 @@
     (parameterize ([current-parser (parser-impl tokens)])
       (rules))))
 
+; thunking `maybe`
+(define-syntax-rule (maybe rule)
+  (maybe-impl (lambda () rule)))
+
+(define (maybe-impl rule)
+  (let ([orig-tokens (get-cur-tokens)])
+    (with-handlers ([exn:parser-error? (lambda (e)
+                                         (set-cur-tokens! orig-tokens)
+                                         #f)])
+      (rule))))
+
 (define (expect token-type)
-  (match (parser-impl-tokens (current-parser))
+  (match (get-cur-tokens)
     [(list-rest (list type value) rest)
      #:when (eq? type token-type)
-     (set-parser-impl-tokens! (current-parser) rest)
+     (set-cur-tokens! rest)
      (list type value) #| pls gief as pattern ;( |#]
     [else (raise (exn:parser-error
                   (string-append "unable to match token: " (symbol->string token-type))
--- tests/parser.rkt
+++ tests/parser.rkt
@@ -4,9 +4,11 @@
 
 (provide parser-tests)
 
+(define lparen-token '(lparen "("))
+(define rparen-token '(rparen ")"))
 (define eq-token '(eq "="))
-(define dash-token '(dash "="))
-(define under-token '(under "="))
+(define dash-token '(dash "-"))
+(define under-token '(under "_"))
 
 (define parser-tests
   (test-suite
@@ -28,4 +30,19 @@
                     (expect 'under)
                     #t))
     (check-equal? #t (parser `(,eq-token ,dash-token ,under-token))))
+
+   (test-case
+    "it can parse maybe tokens"
+    (define parser (make-parser
+                    (expect 'lparen)
+                    (define v (maybe (expect 'eq)))
+                    (expect 'rparen)
+                    v))
+    (check-equal? #f (parser `(,lparen-token ,rparen-token))
+                  "it can still parse the basic version")
+
+    (check-equal? eq-token (parser `(,lparen-token ,eq-token ,rparen-token))
+                  "it can also parse the `maybe` token"))
+
+   ; TODO test failing parser
    ))
