3884d1b39fef139a57c41f827c38bfab659d2c9e
vendethiel <vendethiel@hotmail.fr>
2015-08-13 01:15:04 +0200

    Remove one level of symbol quoting

--- tests/lexer.rkt
+++ tests/lexer.rkt
@@ -18,28 +18,28 @@
 
    (test-case
     "it parses a basic pattern"
-    (check-equal? '(("eq" "="))
-                  (lex '(("=" "eq")) "=")))
+    (check-equal? '((eq "="))
+                  (lex '(("=" eq)) "=")))
 
    (test-case
     "it parses a basic pattern several times"
-    (check-equal? '(("eq" "=") ("eq" "="))
-                  (lex '(("=" "eq")) "==")))
+    (check-equal? '((eq "=") (eq "="))
+                  (lex '(("=" eq)) "==")))
 
    (test-case
     "it parses a space-separated basic pattern several times"
-    (check-equal? '(("eq" "=") ("eq" "="))
-                  (lex '(("=" "eq")) "=  =")))
+    (check-equal? '((eq "=") (eq "="))
+                  (lex '(("=" eq)) "=  =")))
 
    (test-case
     "it parses multiple patterns"
-    (check-equal? '(("eq" "=") ("dash" "-"))
-                  (lex '(("=" "eq") ("-" "dash")) "=-")))
+    (check-equal? '((eq "=") (dash "-"))
+                  (lex '(("=" eq) ("-" dash)) "=-")))
 
    (test-case
     "it captures correctly"
-    (check-equal? '(("eq" "==") ("num" "5"))
-                  (lex '(("=+" "eq") ("[0-9]+" "num")) "== 5")))
+    (check-equal? '((eq "==") (num "5"))
+                  (lex '(("=+" eq) ("[0-9]+" num)) "== 5")))
 
    (test-case
     "it fails to parse invalid code"
