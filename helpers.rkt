4731a83db1ed5876620b12b38d259b9d1c3c3cc8
vendethiel <vendethiel@hotmail.fr>
2015-08-13 17:57:56 +0200

    Add `awhile` and `capturing-awhile` helpers

--- helpers.rkt
+++ helpers.rkt
@@ -1,13 +1,28 @@
 #lang racket
 (require racket/stxparam)
 
-(provide it aif)
+(provide it aif awhile capturing-awhile)
 
 (define-syntax-parameter it
   (lambda (stx)
     (raise-syntax-error #f "`it` outside of `aif`" stx)))
 
 (define-syntax-rule (aif condition then else)
-  (let ([res condition])
-    (syntax-parameterize ([it (make-rename-transformer #'res)])
-      (if res then else))))
+  (let ([tmp condition])
+    (syntax-parameterize ([it (make-rename-transformer #'tmp)])
+      (if tmp then else))))
+
+(define-syntax-rule (awhile condition body ...)
+  (let loop ([tmp condition])
+    (when tmp
+        (syntax-parameterize ([it (make-rename-transformer #'tmp)])
+          body ...)
+        (loop condition))))
+
+(define-syntax-rule (capturing-awhile condition body ...)
+  (let loop ([tmp condition] [results '()])
+    (if tmp
+        (let ([val (syntax-parameterize ([it (make-rename-transformer #'tmp)])
+                     body ...)])
+          (loop condition (cons val results)))
+        (reverse results))))
