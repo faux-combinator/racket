d837839c897da373a073203432a3b4d036b7709c
vendethiel <vendethiel@hotmail.fr>
2015-08-13 18:00:16 +0200

    Add helpers tests to main test file

--- tests.rkt
+++ tests.rkt
@@ -1,7 +1,9 @@
 #lang racket
 (require rackunit rackunit/text-ui)
-(require "tests/lexer.rkt" "tests/parser.rkt")
+(require "tests/helpers.rkt" "tests/lexer.rkt" "tests/parser.rkt")
 
+(display "# helpers\n")
+(run-tests helpers-tests)
 (display "# lexer\n")
 (run-tests lexer-tests)
 (display "# parser\n")
